b1cd23a5eb9877ba63d7650e69a5400434288ce0
FionaJuanY <109893556+FionaJuanY@users.noreply.github.com>
2022-08-11 13:53:07 -0700

    Add files via upload

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup
+
+setup(name='CategoricalCorr',
+      version='1.0',
+      description='All-in-one solution to correlations with categorcial values included',
+      url='https://github.com/FionaJuanY/CategoricalCorr',
+      author='Juan Yu, Varun Kabra',
+      author_email='fionajuanyu@gmail.com',
+      packages=['CategoricalCorr'],
+      zip_safe=False)
